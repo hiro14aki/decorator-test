9458ad7fdb377e1cc713bdbb36def136950af8d0
hiroaki imai <hiro.1114.aki@gmail.com>
2016-11-13 04:07:44 +0900

    Init

---
+++ src/app.js
@@ -0,0 +1,15 @@
+class Fizz {
+  buzz() {
+    return "FizzBuzz";
+  }
+}
+let fizz = new Fizz();
+let res = fizz.buzz();
+console.log(res);
+
+var sampleFunction = () => {
+  var hoge = 'hoge';
+  console.log(hoge);
+};
+
+sampleFunction();
