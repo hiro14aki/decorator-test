61b388d21f39a9f3df6c1a765d55cf2eb677812b
Hiroaki Imai <hiroaki_imai@r.recruit.co.jp>
2016-11-21 17:40:38 +0900

    Delete semicolon.

--- src/app.js
+++ src/app.js
@@ -2,41 +2,41 @@
 // @annotation
 class Person {
   constructor() {
-    this.firstname = "Alice";
-    this.lastname = "Liddell";
+    this.firstname = "Alice"
+    this.lastname = "Liddell"
   }
 
-  @readonly(true)
+  @readonly()
   fullName() {
-    return this.firstname + " " + this.lastname;
+    return this.firstname + " " + this.lastname
   }
 
 }
 
-function readonly(value) {
+function readonly() {
   return function (target, key, descriptor) {
-    descriptor.writable = !value;
-    return descriptor;
+    descriptor.writable = false
+    return descriptor
   }
 }
 
-let person = new Person();
-console.log(person.fullName());
+let person = new Person()
+console.log(person.fullName())
 
-person.fullName = function () {return this.firstname};
-console.log(person.fullName());
-
-
+person.fullName = function () {return this.firstname}
+console.log(person.fullName())
 
 
-@isTestable(true)
-class MyClass { }
 
-function isTestable(value) {
-   return function (target) {
-      target.isTestable = value;
-   }
-}
 
-let myclass = new MyClass();
-console.log(myclass);
+// @isTestable(true)
+// class MyClass { }
+
+// function isTestable(value) {
+//    return function (target) {
+//       target.isTestable = value;
+//    }
+// }
+
+// let myclass = new MyClass();
+// console.log(myclass);
